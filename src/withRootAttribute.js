09fa98c7d37da67014e239db2b24037d3408e9df
Jan Stuhlmann <jan@gridscale.io>
2020-05-18 08:05:22 +0200

    add root2

--- src/withRootAttribute.js
+++ src/withRootAttribute.js
@@ -2,7 +2,7 @@ import document from 'global/document';
 import {EVENTS, PARAM_KEY} from './register/constants';
 import {addons, makeDecorator} from '@storybook/addons';
 
-const updateRootAttribute = ({root = 'html', attribute, currentState}) => {
+const updateRootAttribute = ({root = 'html', attribute, currentState, root2}) => {
   const element = (() => {
     if (root === 'body') {
       return document.body;
@@ -17,6 +17,24 @@ const updateRootAttribute = ({root = 'html', attribute, currentState}) => {
 
   if (currentState.value !== null) {
     element.setAttribute(attribute, currentState.value);
+  }
+
+  if (root2) {
+    const element2 = (() => {
+      if (root2 === 'body') {
+        return document.body;
+      }
+      if (root2 === 'html') {
+        return document.documentElement;
+      }
+      return document.querySelector(root2) || document.documentElement;
+    })();
+
+    element2.removeAttribute(attribute);
+
+    if (currentState.value !== null) {
+      element2.setAttribute(attribute, currentState.value2);
+    }
   }
 };
 
